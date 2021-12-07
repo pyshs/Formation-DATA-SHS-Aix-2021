798115a963763cb20e471c6b7ab3f11f834be45a
Émilien Schultz <emilien.schultz@gmail.com>
2021-12-07 22:07:09 +0100

    Update script.py

--- script.py
+++ script.py
@@ -4,10 +4,10 @@ liste_mots = phrase.split(" ")
 nombre_mots = len(liste_mots)
 nombre_min_lettres = 4
 if nombre_mots > 0:
-    for i in liste_mots:
-        if len(i) < nombre_min_lettres:
+    for i in liste_mots.copy():
+        if len(i) <= nombre_min_lettres:
             liste_mots.remove(i)
 else:
     print("La liste est vide")
-proportion = 100*len(liste_mots)/nombre_mots
+proportion = round(100*len(liste_mots)/nombre_mots,2)
 print("Proportion de mots de plus de {} lettres : {} %".format(nombre_min_lettres,proportion))
